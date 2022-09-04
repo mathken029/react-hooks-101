54234ddd94ef3a8bff5f415fc0b673796cd0e5c6
@_mathken <dummy@example.com>
2022-09-05 08:54:06 +0900

    完成しました。

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useReducer } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import reducer from "../reducers";
+import EventForm from "./EventForm";
+import Events from "./Events";
+import AppContext from "../contexts/AppContext";
+import OperationLogs from "./OperationLogs";
+
+const APP_KEY = "APP_KEY";
+
+const App = () => {
+  const appState = localStorage.getItem(APP_KEY);
+
+  const initialState = appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state));
+  }, [state]);
+
+  return (
+    <>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <div className="container-fluid">
+          <EventForm />
+          <Events />
+          <OperationLogs />
+        </div>
+      </AppContext.Provider>
+    </>
+  );
+};
+
+export default App;
